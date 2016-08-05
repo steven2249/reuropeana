c3467483112d267680fc5da46f5c35278067300e
Scott Chamberlain <myrmecocystus@gmail.com>
2016-08-05 12:13:01 -0700

    remove vignette stuff as no vign. yet, namespace dependency calls

--- R/reuropeana-package.R
+++ R/reuropeana-package.R
@@ -1,7 +1,5 @@
 #' R Client for Europeana.
 #'
-#' @importFrom httr GET content warn_for_status http_status
-#' @importFrom jsonlite fromJSON
 #' @name reuropeana-package
 #' @aliases reuropeana
 #' @docType package
--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 check_response <- function(x) {
   if (!x$status_code == 200) {
     txt <- contutf8(x)
-    if (is(txt, "character")) {
-      stop(http_status(x)$message, call. = FALSE)
+    if (inherits(txt, "character")) {
+      stop(httr::http_status(x)$message, call. = FALSE)
     }
     stnames <- names(jsonlite::fromJSON(txt, FALSE))
     if (!is.null(stnames)) {
@@ -14,7 +14,7 @@ check_response <- function(x) {
         warning(sprintf("Error: (%s)", x$status_code), call. = FALSE)
       }
     } else {
-      warn_for_status(x)
+      httr::warn_for_status(x)
     }
   } else {
     return( x )
@@ -22,21 +22,21 @@ check_response <- function(x) {
 }
 
 eu_GET <- function(url, args, ...) {
-  tt <- GET(url, query = args, ...)
+  tt <- httr::GET(url, query = args, ...)
   check_response(tt)
   stopifnot(tt$headers$`content-type` == "application/json;charset=UTF-8")
   res <- contutf8(tt)
   jsonlite::fromJSON(res, FALSE)
 }
 
-eubase <- function() 'http://europeana.eu/api/v2'
+eubase <- function() 'https://www.europeana.eu/api/v2'
 
 euc <- function(l) Filter(Negate(is.null), l)
 
-contutf8 <- function(x) content(x, "text", encoding = "UTF-8")
+contutf8 <- function(x) httr::content(x, "text", encoding = "UTF-8")
 
 is_error <- function(x) {
-  if (is(x, "error")) {
+  if (inherits(x, "error")) {
     NULL
   } else {
     x
