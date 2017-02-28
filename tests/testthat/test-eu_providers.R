613070956667448caf07f622c76cf24270249a18
steven2249 <stevenysw@berkeley.edu>
2017-02-27 20:13:24 -0800

    Update test-eu_providers.R

--- tests/testthat/test-eu_providers.R
+++ tests/testthat/test-eu_providers.R
@@ -1,6 +1,17 @@
 context("eu_providers")
 
 test_that("eu_providers", {
-  aa <- eu_provides()
+  aa <- eu_provides(key = "sMgTRPQ4Z")
   
-  expect.is(aa, "")
+  expect.is(aa, "list")
+  expect.is(aa$meta, "list")
+  expect.is(aa$meta[[1]], "character")
+  expect.match(aa$meta[[1]], "sMgTRPQ4Z")
+  expect.is(aa$meta[[2]], "logic")
+  expect.is(aa$meta[[3]], "numeric")
+  expect.is(aa$meta[[4]], "integer")
+  expect.is(aa$meta[[5]], "integer")
+  expect.is(aa$meta[[6]], "integer")
+  expect.is(aa$meta[[7]], "integer")
+               
+               })
