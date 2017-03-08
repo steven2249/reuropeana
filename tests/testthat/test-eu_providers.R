a85540f97838fcc8b1686ac5496b6fe70e16bf03
steven2249 <stevenysw@berkeley.edu>
2017-03-07 17:37:15 -0800

    Update test-eu_providers.R

--- tests/testthat/test-eu_providers.R
+++ tests/testthat/test-eu_providers.R
@@ -1,7 +1,7 @@
 context("eu_providers")
 
 test_that("eu_providers", {
-  tt <- eu_providers()
+  tt <- eu_providers(key = getOption("eu_key"))
   
   expect_is(tt, "list")
   expect_is(tt$meta, "list")
@@ -13,4 +13,4 @@ test_that("eu_providers", {
   expect_is(tt$meta[[5]], "integer")
   expect_is(tt$meta[[6]], "integer")
   expect_is(tt$meta[[7]], "integer")
-  })
+})
